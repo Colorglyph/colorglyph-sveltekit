606aa45aa6f0daba404e1425796b64246dae99d6
Tyler van der Hoeven <hi@tyvdh.com>
2023-10-18 15:40:59 -0400

    don't show debug on the live site

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,9 +1,13 @@
 <script>
-    import { page } from '$app/stores';
+    import { dev } from '$app/environment';
+    import { page } from '$app/stores'
 </script>
 
 <nav class="p-1 bg-black mb-1 grid grid-flow-col auto-cols-max gap-x-1">
     <a class="{$page.route.id == '/' ? 'underline text-red-500' : 'text-white'}" href="/">Home</a>
     <a class="{$page.route.id == '/catalog' ? 'underline text-red-500' : 'text-white'}" href="/catalog">Catalog</a>
-    <a class="{$page.route.id == '/debug' ? 'underline text-red-500' : 'text-white'}" href="/debug">Debug</a>
+
+    {#if dev}
+        <a class="{$page.route.id == '/debug' ? 'underline text-red-500' : 'text-white'}" href="/debug">Debug</a>
+    {/if}
 </nav>
