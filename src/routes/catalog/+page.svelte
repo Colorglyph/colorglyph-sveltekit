aa93121d4d3dc93124872a818141adea8382dbb6
Tyler van der Hoeven <hi@tyvdh.com>
2024-09-05 12:58:53 -0400

    tweak the Catalog

--- src/routes/catalog/+page.svelte
+++ src/routes/catalog/+page.svelte
@@ -5,24 +5,9 @@
 
     const api = fetcher({ base: API_BASE });
 
-    // interface Glyph {
-    //     name: string;
-    //     metadata: {
-    //         id: string;
-    //         fee: string;
-    //         status: string;
-    //         width: number;
-    //         length: number;
-    //         mishash: string;
-    //     };
-    // }
-
     interface Glyph {
         Hash: string;
-        Owner: string;
-        Minter: string;
-        Width: number;
-        Length: number;
+        Id: string;
     }
 
     let glyphs: Glyph[] = [];
@@ -38,32 +23,30 @@
         });
     }
     async function getStatus(glyph: Glyph) {
-    //     console.log(glyph);
+        console.log(glyph);
 
-    //     await api.get(`/mint/${glyph.metadata.id}`).then((res: any) => {
-    //         console.log(res);
-    //         glyph.metadata.status = res.status ?? glyph.metadata.status;
-    //         glyphs = glyphs;
-    //     });
+        await api.get(`/mint/${glyph.Id}`).then((res: any) => {
+            console.log(res);
+            // glyph.metadata.status = res.status ?? glyph.metadata.status;
+            // glyphs = glyphs;
+        });
     }
 </script>
-
-<!-- {glyph
-    .metadata.status === 'mining'
-    ? 'bg-red-500'
-    : glyph.metadata.status === 'minting'
-      ? 'bg-yellow-500'
-      : glyph.metadata.status === 'complete' ||
-          glyph.metadata.status === 'minted'
-        ? 'bg-green-500'
-        : 'bg-black'}  -->
 
 <div class="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-12 gap-1">
     {#each glyphs as glyph}
         <div class="relative" on:click={() => getStatus(glyph)}>
-            <span
-                class="w-3 h-3 rounded-full absolute top-1 left-1 border-2 border-white"
-            ></span>
+            <!-- <span
+                class="w-3 h-3 rounded-full absolute top-1 left-1 border-2 border-white {glyph
+                    .metadata.status === 'mining'
+                    ? 'bg-red-500'
+                    : glyph.metadata.status === 'minting'
+                      ? 'bg-yellow-500'
+                      : glyph.metadata.status === 'complete' ||
+                          glyph.metadata.status === 'minted'
+                        ? 'bg-green-500'
+                        : 'bg-black'}"
+            ></span> -->
             <img
                 class="w-full rendering-pixelated"
                 src="{API_BASE}/image/{glyph.Hash}"
